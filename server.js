3d5e6ced080449c8725ae759aa0e2f75da2c43dd
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-04-26 19:25:11 -0500

    added session and store

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-// Boiler plate from Act. 14.7
-
+// Boiler plate from Act. 14.17
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
