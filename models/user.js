efaa54c3c657e49bca12b28a7d04df03eaebcd47
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-04-26 19:18:34 -0500

    user model boiler

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('./config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
+
+
+  module.exports = User;
