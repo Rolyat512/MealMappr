b093cca02b6fc166be0c59f13dfc3eee5359ac55
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-04-26 21:03:51 -0500

    model/user boiler

--- models/user.js
+++ models/user.js
@@ -8,5 +8,50 @@ class User extends Model {
     }
   }
 
+  // Boiler plate from 14.21 said to copy/paste basically 
 
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8],
+        },
+      },
+    },
+    {
+      hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
+  
   module.exports = User;
+  
