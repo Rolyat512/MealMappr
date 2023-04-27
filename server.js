a317c533061bdd0a645057327e7ad2a6c9508579
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-04-26 19:32:32 -0500

    more boiler plate

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -17,6 +17,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/dish-routes'));
+// need to update this ^ 
+
+app.use(session(sess));
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 15 * 60 * 1000, 
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
