82f75630eb24a8cdb0473394a35360b9bd0e7dd8
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-04-26 20:57:11 -0500

    added util auth boiler

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const withAuth = async (req, res, next) => {
+    // If the user is not logged in, redirect the user to the login page
+    if (!req.session.loggedIn) {
+    res.redirect('/login');
+    return;
+    }
+    next();
+    };
+  
+  
+  module.exports = withAuth;
