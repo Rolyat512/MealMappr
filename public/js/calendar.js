996c054c99f1bce132c4e36d22d3a8cc447d9da4
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-05-03 19:28:56 -0500

    work in progress

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,2 +1,10 @@
+$(document).ready(function () {
+    var calendar = $('#calendar');
+    var calendar = new FullCalendar.Calendar(calendar[0], {
+      initialView: 'dayGridMonth',
+      // Add other options and event data here
+    });
+    calendar.render();
+  });
 
-
+  
