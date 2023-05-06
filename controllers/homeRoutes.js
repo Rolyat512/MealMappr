18d6364172f33e530738075808bc0036506d6a56
Omar4589 <omar4589@hotmail.com>
2023-05-05 21:48:41 -0500

    adds settings View and report and issue View

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,6 +28,26 @@ router.get("/home", withAuth, async (req, res) => {
   }
 });
 
+router.get("/reportanissue", async (req, res) => {
+  try {
+    //res.json({message: 'This will be the home page after user is logged in'} )
+    res.render("reportissue", { isLoggedIn: req.session.loggedIn }); //this will be for redner the home handlebars layout
+  } catch (err) {
+    res.status(400).json({ message: "No homepage found" });
+    console.log(err);
+  }
+});
+
+router.get("/settings", withAuth, async (req, res) => {
+  try {
+    //res.json({message: 'This will be the home page after user is logged in'} )
+    res.render("settings", { isLoggedIn: req.session.loggedIn }); //this will be for redner the home handlebars layout
+  } catch (err) {
+    res.status(400).json({ message: "No homepage found" });
+    console.log(err);
+  }
+});
+
 // when user clicks the acount settings button
 // router.get('/home/settings', withAuth, async (req, res) => {
 //     try {
